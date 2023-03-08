13549b0b646cf4b793afbb3063101946e02c66d1
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-03-07 23:58:17 -0300

    Correct customers form and registered message

--- Vidly/Persistence/VidlyDbContext.cs
+++ Vidly/Persistence/VidlyDbContext.cs
@@ -9,6 +9,7 @@ namespace Vidly.Persistence
         {} 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipType { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
     }
 }
